acedcb4a76a51b964465eb3c94dd4277e5777ef3
Smallorange <1716070769@qq.com>
2024-03-20 20:36:57 +0800

    finish lab1

--- pkg/boot/src/main.rs
+++ pkg/boot/src/main.rs
@@ -1,27 +1,123 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 extern crate log;
 extern crate alloc;
 
-use core::arch::asm;
+use alloc::boxed::Box;
+use alloc::vec;
+use elf::{load_elf, map_physical_memory, map_range};
 use uefi::prelude::*;
+use x86_64::registers::control::*;
+use ysos_boot::*;
+
+mod config;
+
+const CONFIG_PATH: &str = "\\EFI\\BOOT\\boot.conf";
 
 #[entry]
 fn efi_main(image: uefi::Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).expect("Failed to initialize utilities");
+
     log::set_max_level(log::LevelFilter::Info);
+    info!("Running UEFI bootloader...");
 
-    let std_num = 22336152;
+    let bs = system_table.boot_services();
 
-    loop {
-        info!("Hello World from UEFI bootloader! @ {}", std_num);
+    // Load config
+    let config = {
+        let mut file = open_file(bs, CONFIG_PATH);
+        config::Config::parse(load_file(bs, &mut file))
+    };
 
-        for _ in 0..0x10000000 {
-            unsafe {
-                asm!("nop");
-            }
-        }
+    info!("Config: {:#x?}", config);
+
+    // Load ELF files
+    let elf = {
+        let mut file = open_file(bs, config.kernel_path);
+        xmas_elf::ElfFile::new(load_file(bs, &mut file)).unwrap()
+    };
+
+    unsafe {
+        set_entry(elf.header.pt2.entry_point() as usize);
+    }
+
+    // Load MemoryMap
+    let max_mmap_size = system_table.boot_services().memory_map_size();
+    let mmap_storage = Box::leak(
+        vec![0; max_mmap_size.map_size + 10 * max_mmap_size.entry_size].into_boxed_slice(),
+    );
+    let mmap = system_table
+        .boot_services()
+        .memory_map(mmap_storage)
+        .expect("Failed to get memory map");
+
+    let max_phys_addr = mmap
+        .entries()
+        .map(|m| m.phys_start + m.page_count * 0x1000)
+        .max()
+        .unwrap()
+        .max(0x1_0000_0000); // include IOAPIC MMIO area
+
+    // Map ELF segments, kernel stack and physical memory to virtual memory
+    let mut page_table = current_page_table();
+
+    // Root page table is read only, disable write protect (Cr0)
+    unsafe { Cr0::update(|f| f.remove(Cr0Flags::WRITE_PROTECT)) }
+
+    // Map physical memory to specific virtual address offset
+    let mut frame_allocator = UEFIFrameAllocator(bs);
+    map_physical_memory(
+        config.physical_memory_offset,
+        max_phys_addr,
+        &mut page_table,
+        &mut frame_allocator,
+    );
+
+    // Load and map the kernel elf file
+    load_elf(
+        &elf,
+        config.physical_memory_offset,
+        &mut page_table,
+        &mut frame_allocator,
+    )
+    .expect("");
+
+    // Map kernel stack
+    map_range(
+        config.kernel_stack_address,
+        config.kernel_stack_size,
+        &mut page_table,
+        &mut frame_allocator,
+    )
+    .expect("");
+
+    // Recover write protect (Cr0)
+    unsafe {
+        Cr0::update(|f| f.insert(Cr0Flags::WRITE_PROTECT));
+    }
+
+    free_elf(bs, elf);
+
+    // Exit boot and jump to ELF entry
+    info!("Exiting boot services...");
+
+    let (runtime, mmap) = system_table.exit_boot_services(MemoryType::LOADER_DATA);
+    // NOTE: alloc & log are no longer available
+
+    // construct BootInfo
+    let bootinfo = BootInfo {
+        memory_map: mmap.entries().copied().collect(),
+        physical_memory_offset: config.physical_memory_offset,
+        system_table: runtime,
+    };
+
+    // align stack to 8 bytes
+    let stacktop = config.kernel_stack_address + config.kernel_stack_size * 0x1000 - 8;
+
+    unsafe {
+        jump_to_entry(&bootinfo, stacktop);
     }
 }
---
+++ pkg/kernel/src/drivers/uart16550.rs
@@ -0,0 +1,67 @@
+use core::fmt;
+use x86_64::instructions::port::Port;
+
+/// A port-mapped UART 16550 serial interface.
+pub struct SerialPort {
+    data: Port<u8>,
+    interrupt_enable: Port<u8>,
+    interrupt_identification_fifo_control: Port<u8>,
+    line_control: Port<u8>,
+    line_status: Port<u8>,
+    scratch: Port<u8>,
+}
+
+impl SerialPort {
+    pub const fn new(port: u16) -> Self {
+        Self {
+            data: Port::new(port),
+            interrupt_enable: Port::new(port + 1),
+            interrupt_identification_fifo_control: Port::new(port + 2),
+            line_control: Port::new(port + 3),
+            line_status: Port::new(port + 5),
+            scratch: Port::new(port + 7),
+        }
+    }
+
+    /// Initializes the serial port.
+    pub fn init(&mut self) {
+        unsafe {
+            self.interrupt_enable.write(0x00); // Disable all interrupts
+            self.line_control.write(0x80); // Enable DLAB (set baud rate divisor)
+            self.data.write(0x03); // Set divisor to 3 (lo byte) 38400 baud
+            self.interrupt_enable.write(0x00); // Set divisor to 3 (hi byte) 38400 baud
+            self.line_control.write(0x03); // 8 bits, no parity, one stop bit
+            self.interrupt_identification_fifo_control.write(0xC7); // Enable FIFO, clear them, with 14-byte threshold
+            self.scratch.write(0xAE);
+            self.interrupt_enable.write(0x01);
+        }
+    }
+
+    /// Sends a byte on the serial port.
+    pub fn send(&mut self, data: u8) {
+        unsafe {
+            while self.line_status.read() & 0x20 == 0 {}
+            self.data.write(data);
+        }
+    }
+
+    /// Receives a byte on the serial port no wait.
+    pub fn receive(&mut self) -> Option<u8> {
+        unsafe {
+            if self.line_status.read() & 1 != 0 {
+                Some(self.data.read())
+            } else {
+                None
+            }
+        }
+    }
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for byte in s.bytes() {
+            self.send(byte);
+        }
+        Ok(())
+    }
+}
